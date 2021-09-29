fe91b912bae247cddda153457f07886bbc831e44
Yasuo <xianzhengquan@tinman.cn>
2021-09-29 14:59:46 +0800

    fix: rollup 打包之后 esm 文件报错

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,45 @@
 import nodeResolve from '@rollup/plugin-node-resolve'; // 编译第三方包
+// import nodePolyfills from 'rollup-plugin-polyfill-node';
 import typescript from 'rollup-plugin-typescript2'; // 编译ts
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
-import builtins from 'rollup-plugin-node-builtins';
+import commonjs from '@rollup/plugin-commonjs'; // 可以打包 commonjs
+// import babel from '@rollup/plugin-babel';
+// import builtins from 'rollup-plugin-node-builtins';
 import { terser } from 'rollup-plugin-terser';
 import path from 'path';
 import pkg from './package.json';
 
 const resolve = (p) => path.resolve(__dirname, p); // 适应不同环境，封装path.resolve，少写一点代码
+const extensions = ['.tsx', '.ts', '.jsx', '.mjs', '.js', '.json', '.node'];  // 默认查找的文件扩展名
 
 export default {
   input: resolve(pkg.entry),
+  external: Object.keys(pkg.devDependencies),
   output: [
     {
       file: resolve(pkg.main),
       format: 'cjs', // commonjs 形式的包， require 导入
-      exports: 'named'
+      plugins: [terser({ toplevel: true })]
     },
     {
       file: resolve(pkg.module),
       format: 'es',  // es module 形式的包， 用来import 导入， 可以tree shaking
-      exports: 'named'
+      plugins: [terser({ module: true })]
     },
     {
-      file: 'dist/index.umd.js',
+      file: resolve(pkg.unpkg),
       name: 'Json2Canvas',
-      format: 'umd', // umd 兼容形式的包， 可以直接应用于网页 script
-      exports: 'named'
+      format: 'umd' // umd 兼容形式的包， 可以直接应用于网页 script
     }
   ],
   plugins: [
-    builtins(),
+    nodeResolve({
+      browser: true,
+      extensions: extensions
+    }),
+    // babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
     commonjs(),
-    babel({ babelHelpers: 'bundled' }),
-    nodeResolve(),
-    typescript(),
-    terser()
+    // builtins(),
+    // nodePolyfills(),
+    typescript()
   ]
 };
